48e22df9817d29ee302b7d02eb733191baa73aa9
Aslan <berezg86@gmail.com>
2020-11-05 12:24:27 +0300

    Шапка, верхний блок на главной

--- src/static/js/common.js
+++ src/static/js/common.js
@@ -1,3 +1,30 @@
 $(document).ready(function () {
-    // alert(1);
+    $('.burger').click(function () {
+        $(this).toggleClass('is-active');
+        $('.dropdownmenu').toggleClass('is-active');
+    });
+
+    $('.custom-select select').selectric();
+
+    var contSlider = new Swiper('.reserv-gallery', {
+        slidesPerView: 1,
+        spaceBetween: 10,
+        loop: true,
+        observer: true,
+        observeParents: true,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        breakpoints: {
+            767: {
+                slidesPerView: 1.2,
+                spaceBetween: 19,
+            }
+        }
+    });
 });
